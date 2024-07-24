0c098a33a7ad77de445902841a9d062ed7a05c31
Shushant <110722595+shiinu2209@users.noreply.github.com>
2024-07-25 04:29:45 +0530

    Update index.js
    adding port to env

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ app.use(express.json());
 app.use("/", videoRoutes);
 app.use("/", userRoutes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
